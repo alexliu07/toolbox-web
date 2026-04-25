a17357d0b9d3859b3bcf8429e132bfc1481bf059
leohu1 <2607131406@qq.com>
2026-04-25 19:53:31 +0800

    Markdown编辑器完成

---
+++ src/components/MarkdownEditor.vue
@@ -0,0 +1,72 @@
+<script setup>
+import Vditor from "vditor"
+import 'vditor/dist/index.css';
+
+import {onMounted, ref} from 'vue'
+
+const editor = ref();
+const props = defineProps({
+  fileUrl:  { type: String, required: true },
+  fileName: { type: String, default: '' },
+})
+
+onMounted(() => {
+  let contentEditor = new Vditor(editor.value, {
+    height: 650,
+    toolbarConfig: {
+      pin: true,
+    },
+    cache: {
+      enable: false,
+    },
+    toolbar: ["emoji",
+      "headings",
+      "bold",
+      "italic",
+      "strike",
+      "|",
+      "list",
+      "ordered-list",
+      "check",
+      "outdent",
+      "indent",
+      "|",
+      "quote",
+      "line",
+      "code",
+      "inline-code",
+      "insert-before",
+      "insert-after",
+      "|",
+      "table",
+      "|",
+      "undo",
+      "redo",
+      "|",
+      "fullscreen",
+      "edit-mode",
+      {
+        name: "more",
+        toolbar: [
+          "both",
+          "code-theme",
+          "content-theme",
+          "outline",
+        ],
+      },],
+    cdn: "/vditor",
+    after: () => {
+      contentEditor.setTheme("dark", "dark", "fruity");
+      contentEditor.setValue('hello, Vditor + Vue!')
+    },
+  })
+})
+</script>
+
+<template>
+  <div ref="editor"></div>
+</template>
+
+<style scoped>
+
+</style>
